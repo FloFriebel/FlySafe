155860402bba49b146af95e2de8883cbe72e76b6
Florence Friebel <Florence.Friebel@proton.me>
2024-03-22 18:08:11 +0100

    creating packaging for Api

--- project_notebooks/data_preprocessing/old_load_data.py
+++ project_notebooks/data_preprocessing/old_load_data.py
@@ -6,7 +6,7 @@ from retry_requests import retry
 
 
 # Setup the Open-Meteo API client with cache and retry on error
-def load_data(_start, _end):
+def load_data(_start, _end, _city):
     cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
     retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
     openmeteo = openmeteo_requests.Client(session = retry_session)
@@ -24,33 +24,49 @@ def load_data(_start, _end):
     # Process daily data. The order of variables needs to be the same as requested.
     responses = openmeteo.weather_api(url, params=params)
     data_list = []
-    city_codes = {"Zurich": 'zh', "Lugano": 'lu', "Innsbruck": 'in', "Bolzano": 'bo'}
-    for city, response in zip(("Zurich", "Lugano", "Innsbruck", "Bolzano"), responses):
-   # """ city_query = [0, 1, 2, 3]
-   # print(city_query) """
-    #for city in city_query:
-        #response = responses[city] #append function to an empty list
-        hourly = response.Hourly()
-        _start = pd.to_datetime(hourly.Time(), unit = "s", utc = True)
-        _end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True)
-        _delta = pd.Timedelta(seconds = hourly.Interval())
+    if _city == "Zurich":
+        city_query = [0, 1]
+        print(city_query)
+        for _city in city_query:
+            response = responses[_city] #append function to an empty list
+            hourly = response.Hourly()
+            _start = pd.to_datetime(hourly.Time(), unit = "s", utc = True)
+            _end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True)
+            _delta = pd.Timedelta(seconds = hourly.Interval())
 
-        data = {
-            "date": pd.date_range(_start, _end, freq=_delta, inclusive="left"),
-            "temperature_2m": hourly.Variables(0).ValuesAsNumpy(),
-            "surface_pressure": hourly.Variables(1).ValuesAsNumpy(),
-            "wind_speed_10m": hourly.Variables(2).ValuesAsNumpy(),
-            "wind_direction_10m": hourly.Variables(3).ValuesAsNumpy(),
-            "wind_gusts_10m": hourly.Variables(4).ValuesAsNumpy(),
-        }
-        city_code = city_codes[city]
-        data_list.append(pd.DataFrame(data).set_index("date").add_suffix(f"_{city_code}"))
+            data = {
+                "date": pd.date_range(_start, _end, freq=_delta, inclusive="left"),
+                "temperature_2m": hourly.Variables(0).ValuesAsNumpy(),
+                "surface_pressure": hourly.Variables(1).ValuesAsNumpy(),
+                "wind_speed_10m": hourly.Variables(2).ValuesAsNumpy(),
+                "wind_direction_10m": hourly.Variables(3).ValuesAsNumpy(),
+                "wind_gusts_10m": hourly.Variables(4).ValuesAsNumpy(),
+            }
+            data_list.append(pd.DataFrame(data).set_index("date"))
+    elif _city == "Innsbruck":
+        city_query = [2, 3]
+        print(city_query)
+        for _city in city_query:
+            response = responses[_city] #append function to an empty list
+            hourly = response.Hourly()
+            _start = pd.to_datetime(hourly.Time(), unit = "s", utc = True)
+            _end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True)
+            _delta = pd.Timedelta(seconds = hourly.Interval())
 
-    correct = pd.concat(data_list, axis=1)
-    return correct   # should return df 840 X 20 for _start & _end "2024-02-10", "2024-03-15"
+            data = {
+                "date": pd.date_range(_start, _end, freq=_delta, inclusive="left"),
+                "temperature_2m": hourly.Variables(0).ValuesAsNumpy(),
+                "surface_pressure": hourly.Variables(1).ValuesAsNumpy(),
+                "wind_speed_10m": hourly.Variables(2).ValuesAsNumpy(),
+                "wind_direction_10m": hourly.Variables(3).ValuesAsNumpy(),
+                "wind_gusts_10m": hourly.Variables(4).ValuesAsNumpy(),
+            }
+            data_list.append(pd.DataFrame(data).set_index("date"))
+    data_combined = pd.concat(data_list, axis=1)
+    return data_combined
 # Example
 if __name__=="__main__":
-    data_new = load_data("2024-02-10", "2024-03-15")
+    data_new = load_data("2024-02-10", "2024-03-15", "Zurich")
     print(data_new)
 
 
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='jawP', packages=find_packages(), install_requires=requirements)
